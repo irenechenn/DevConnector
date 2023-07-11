a3a873bb5933027f26ae6bcc9fabb696a4388f6e
Irene <73439899+irenechenn@users.noreply.github.com>
2023-07-10 20:26:37 -0700

    second commit

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 const express = require('express');
+const connectDB = require('./config/db');
 
 const app = express();
-
+//Connect Database
+connectDB();
 // create a single end point to test out
 app.get('/', (req, res) => res.send('API running'));
 
